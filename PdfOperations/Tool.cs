66a0cfb4d24ddd18b47ff124402faa6f9da443c0
DanBed666 <danielbb0098@gmail.com>
2026-07-01 11:23:54 +0200

    Add tool paths

---
+++ PdfOperations/Tool.cs
@@ -0,0 +1,14 @@
+﻿namespace PdfOperations;
+
+public enum Tool
+{
+    LibreOffice,
+    PdfToPpm,
+    Magick,
+    PdfUnite,
+    PdfToText,
+    Tesseract,
+    Qpdf,
+    PdfInfo,
+    PdfSeparate
+}
---
+++ PdfOperations/ToolPaths.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PdfOperations;
+
+public class ToolPaths
+{
+    public static readonly string ToolsDir = Path.Combine(AppContext.BaseDirectory, "tools");
+
+    private static readonly IReadOnlyDictionary<Tool, string> ToolPathsDict =
+        new ReadOnlyDictionary<Tool, string>(new Dictionary<Tool, string>
+            {
+                [Tool.LibreOffice] = Path.Combine(ToolsDir, "LibreOfficePortable", "App",
+                        "libreoffice", "program", "soffice.com"),
+                [Tool.PdfToPpm] = Path.Combine(ToolsDir, "poppler-26.02.0", "Library",
+                    "bin", "pdftoppm.exe"),
+                [Tool.Magick] = Path.Combine(ToolsDir, "ImageMagick-7.1.2-26-portable-Q16-x64", 
+                    "magick.exe"),
+                [Tool.PdfUnite] = Path.Combine(ToolsDir, "poppler-26.02.0", "Library",
+                    "bin", "pdfunite.exe"),
+                [Tool.PdfToText] = Path.Combine(ToolsDir, "poppler-26.02.0", "Library",
+                    "bin", "pdftotext.exe"),
+                [Tool.Tesseract] = Path.Combine(ToolsDir, "Tesseract-OCR", "tesseract.exe"),
+                [Tool.Qpdf] = Path.Combine(ToolsDir, "qpdf-12.3.2-msvc64", "bin", "qpdf.exe"),
+                [Tool.PdfInfo] = Path.Combine(ToolsDir, "poppler-26.02.0", "Library",
+                    "bin", "pdfinfo.exe"), 
+                [Tool.PdfSeparate] = Path.Combine(ToolsDir, "poppler-26.02.0", "Library",
+                    "bin", "pdfseparate.exe"),
+            }
+        );
+}
