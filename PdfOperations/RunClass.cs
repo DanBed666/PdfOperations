3e0c9708cb3fdb3f28d821efb5b02bee9cee6ee8
DanBed666 <danielbb0098@gmail.com>
2026-07-01 09:56:02 +0200

    Add running exe from libraries

---
+++ PdfOperations/RunClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace PdfOperations;
+
+public class RunClass
+{
+    public static void Run(string exe, params  string[] arguments)
+    {
+        var info = new ProcessStartInfo
+        {
+            FileName = exe,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+
+        foreach (string argument in arguments)
+        {
+            info.ArgumentList.Add(argument);
+        }
+        
+        using Process process = Process.Start(info)!;
+        process.WaitForExit();
+    }
+}
