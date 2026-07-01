4b8f998366ec2f2ca5abb16eff79c5f08b530bbe
DanBed666 <danielbb0098@gmail.com>
2026-07-01 08:55:11 +0200

    Add main menu switch options

--- PdfOperations/Program.cs
+++ PdfOperations/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        while (true)
+        {
+            Console.WriteLine("1. Plik na Pdf");
+            Console.WriteLine("2. Pdf na obraz");
+            Console.WriteLine("3. Obraz na tekst");
+            Console.WriteLine("4. Pdf na text");
+            Console.WriteLine("5. Menu glowne");
+            Console.WriteLine("0. Wyjscie");
+            Console.WriteLine("Wpisz opcje: ");
+            string znak = Console.ReadLine();
+            
+            switch (znak)
+            {
+                case "1":
+                    Console.WriteLine(1);
+                    break;    
+                case "2":
+                    Console.WriteLine(2);
+                    break; 
+                case "3":
+                    Console.WriteLine(3);
+                    break; 
+                case "4":
+                    Console.WriteLine(4);
+                    break; 
+                case "5":
+                    Console.WriteLine(5);
+                    break; 
+                case "0":
+                    Console.WriteLine(0);
+                    break; 
+                default:
+                    Console.WriteLine("default");
+                    break; 
+            }
+        }
     }
 }
