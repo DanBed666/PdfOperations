b12c6f58d6a2f50978b4a040168c3c24edb46acf
DanBed666 <danielbb0098@gmail.com>
2026-07-02 09:54:28 +0200

    Add files and directories dialogs

--- PdfOperations/Dialog.cs
+++ PdfOperations/Dialog.cs
@@ -6,7 +6,7 @@ public class Dialog
     {
         using var dialog = new OpenFileDialog
         {
-            Title = "Nowe okno",
+            Title = "Wybierz plik",
             Filter = "Pliki PDF (*.pdf)|*.pdf|Wszystkie pliki (*.*)|*.*",
             AutoUpgradeEnabled = true,
             RestoreDirectory = true,
@@ -14,5 +14,41 @@ public class Dialog
         };
         
         return dialog.ShowDialog() == DialogResult.OK ? dialog.FileName : null;
+    }
+    
+    public static string [] SelectFiles()
+    {
+        using var dialog = new OpenFileDialog
+        {
+            Title = "Wybierz pliki",
+            Filter = "Pliki PDF (*.pdf)|*.pdf|Wszystkie pliki (*.*)|*.*",
+            AutoUpgradeEnabled = true,
+            RestoreDirectory = true,
+            Multiselect = true
+        };
+        
+        return dialog.ShowDialog() == DialogResult.OK ? dialog.FileNames : null;
+    }
+    
+    public static string SelectDirectory()
+    {
+        using var dialog = new FolderBrowserDialog()
+        {
+            Description = "Wybierz folder",
+            ShowNewFolderButton = true,
+        };
+        
+        return dialog.ShowDialog() == DialogResult.OK ? dialog.SelectedPath : null;
+    }
+    
+    public static string [] SelectDirectories()
+    {
+        using var dialog = new FolderBrowserDialog()
+        {
+            Description = "Wybierz foldery",
+            ShowNewFolderButton = true,
+        };
+        
+        return dialog.ShowDialog() == DialogResult.OK ? dialog.SelectedPaths : null;
     }
 }
