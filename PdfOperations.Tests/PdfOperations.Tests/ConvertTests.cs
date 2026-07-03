1efcbe8ad4996befdf4ba89480bdd72adddd1b22
DanBed666 <danielbb0098@gmail.com>
2026-07-03 14:00:27 +0200

    Add file to pdf test method

--- PdfOperations.Tests/PdfOperations.Tests/ConvertTests.cs
+++ PdfOperations.Tests/PdfOperations.Tests/ConvertTests.cs
@@ -4,9 +4,43 @@
 public class ConvertTests()
 {
     [TestMethod]
-    public void FileToPdfTest(string [] files)
+    public void FileToPdfTest()
     {
-        //Convert.FileToPdf();
+        string input = Path.Combine(AppContext.BaseDirectory, "TestData", "word.docx");
+        string input2 = Path.Combine(AppContext.BaseDirectory, "TestData", "word2.docx");
+        string input3 = Path.Combine(AppContext.BaseDirectory, "TestData", "word3.docx");
+
+        string [] inputFiles = new []{input, input2, input3};
+        string [] outputFiles = new string[inputFiles.LongLength];
+
+        for (int i = 0; i < inputFiles.Length; i++)
+        {
+            outputFiles[i] = inputFiles[i].Replace(".docx", ".pdf");
+        }
+
+        try
+        {
+            Convert.FileToPdf(inputFiles);
+            Assert.HasCount(3, outputFiles);
+
+            for (int i = 0; i < outputFiles.Length; i++)
+            {
+                Assert.IsTrue(File.Exists(outputFiles[i]));
+                Assert.IsGreaterThan(0, new FileInfo(outputFiles[i]).Length);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            for (int i = 0; i < outputFiles.Length; i++)
+            {
+                File.Delete(outputFiles[i]);
+            }
+        }
     }
     
     [TestMethod]
@@ -39,9 +73,28 @@ public class ConvertTests()
     }
     
     [TestMethod]
-    public void PictToTxtTest(string input, string output)
+    public void PictToTxtTest()
     {
-        //Convert.PictToTxt();
+        string input = Path.Combine(AppContext.BaseDirectory, "TestData", "obraz.png");
+        string output = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}");
+        
+        string outputTest = output + ".txt";
+
+        try
+        {
+            Convert.PictToTxt(input, output);
+            Assert.IsTrue(File.Exists(outputTest));
+            Assert.IsGreaterThan(0, new FileInfo(outputTest).Length);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            File.Delete(output);
+        }
     }
     
     [TestMethod]
