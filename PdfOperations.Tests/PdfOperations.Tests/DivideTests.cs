d2a519d792e3ee4ff4636cb253591f4d91bfd819
DanBed666 <danielbb0098@gmail.com>
2026-07-03 14:56:24 +0200

    Add sepaprate pdf test method

--- PdfOperations.Tests/PdfOperations.Tests/DivideTests.cs
+++ PdfOperations.Tests/PdfOperations.Tests/DivideTests.cs
@@ -4,8 +4,38 @@
 public class DivideTests
 {
     [TestMethod]
-    public void OneToMany(string input, string output)
+    public void OneToMany()
     {
+        string input = Path.Combine(AppContext.BaseDirectory, "TestData", "pdf_test.pdf");
+        string dir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(dir);
+        string output = Path.Combine(dir, $"test8-%d.pdf");
+
+        try
+        {
+            Divide.OneToMany(input, output);
+
+            string [] outputFiles = Directory.GetFiles(dir, "test8-*.pdf");
+            Assert.HasCount(6, outputFiles);
+
+            for (int page = 1; page <= 6; page++)
+            {
+                string outputFile = Path.Combine(dir, $"test8-{page}.pdf");
+                
+                Assert.IsTrue(File.Exists(outputFile));
+                Assert.IsGreaterThan(0, new FileInfo(outputFile).Length);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            Directory.Delete(dir, true);
+        }
+        
         //Divide.OneToMany();
     }
     
