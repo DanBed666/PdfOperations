42ad203f588bf4a468ea6963ee65b0a3d1189702
DanBed666 <danielbb0098@gmail.com>
2026-07-03 09:25:04 +0200

    Add tests project (again) and delete wrong project

--- PdfOperations.Tests/PdfOperations.Tests/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PdfOperations.Tests;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
---
+++ PdfOperations.Tests/PdfOperations.Tests/Test1.cs
@@ -0,0 +1,12 @@
+﻿using PdfOperations;
+namespace PdfOperations.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        
+    }
+}
