fb17e2151472e5ac9977da3815ccd5ef6c5e4a49
DanBed666 <danielbb0098@gmail.com>
2026-07-13 14:25:27 +0200

    Add handling console input methods

---
+++ PdfOperations/Infrastructure/ReadInput.cs
@@ -0,0 +1,48 @@
+﻿namespace PdfOperations;
+
+public class ReadInput
+{
+    public static string ReadOutputFile()
+    {
+        string input = Console.ReadLine()!;
+
+        if (input.Trim().Equals(":q", StringComparison.OrdinalIgnoreCase))
+        {
+            return null;
+        }
+
+        if (string.IsNullOrEmpty(input))
+        {
+            return "default";
+        }
+
+        return input;
+    }
+
+    public static string ReadOption()
+    {
+        string input = Console.ReadLine()!;
+        
+        while (string.IsNullOrEmpty(input))
+        {
+            if (input.Trim().Equals(":q", StringComparison.OrdinalIgnoreCase))
+            {
+                return null;
+            }
+            
+            if (input.Trim().Equals("t", StringComparison.OrdinalIgnoreCase))
+            {
+                return "t";
+            }
+            
+            if (input.Trim().Equals("n", StringComparison.OrdinalIgnoreCase))
+            {
+                return "n";
+            }
+            
+            Console.WriteLine("Niepoprawna opcja!");
+        }
+
+        return input;
+    }
+}
