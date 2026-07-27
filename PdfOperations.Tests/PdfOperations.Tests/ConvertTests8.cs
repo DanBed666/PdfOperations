266c685c946bc9f1c98ddd52f3ac971e51e67cda
DanBed666 <danielbb0098@gmail.com>
2026-07-27 14:31:37 +0200

    Update conversion tests

---
+++ PdfOperations.Tests/PdfOperations.Tests/ConvertTests8.cs
@@ -0,0 +1,278 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace PdfOperations.Tests;
+
+[TestClass]
+public class ConvertTests8()
+{
+    [TestMethod]
+    public void FileToPdfTest()
+    {
+        string dir = Path.Combine(AppContext.BaseDirectory, "TestData");
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(tempDir);
+        
+        string inputPath = Path.Combine(dir, "word.docx");
+        string inputPath2 = Path.Combine(dir, "kompy.docx");
+        string inputPath3 = Path.Combine(dir, "pdf_test.docx");
+        string[] inputs = new[] { inputPath, inputPath2, inputPath3 };
+        
+        string format = ".pdf";
+
+        InputClass input = new InputClass
+        {
+            inputFiles = inputs,
+            phrase = "pdf",
+            dir = tempDir
+        };
+        
+        try
+        {
+            Convert.FileToPdf(input);
+            Assert.HasCount(3, Directory.GetFiles(tempDir));
+
+            foreach (string file in Directory.GetFiles(tempDir))
+            {
+                Assert.IsTrue(File.Exists(file));
+                Assert.IsGreaterThan(0, new FileInfo(file).Length);
+            }
+        }
+        catch (Exception e)
+        {
+            ErrorLogger.Log(e);
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        finally
+        {
+            if (Directory.Exists(tempDir))
+                Directory.Delete(tempDir, true);
+        }
+    }
+    
+    [TestMethod]
+    public void PdfToPictTest()
+    {
+        string dir = Path.Combine(AppContext.BaseDirectory, "TestData");
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(tempDir);
+        List<InputClass> inputFiles = new List<InputClass>();
+        string inputPath = Path.Combine(dir, "ocr_test_1.pdf");
+        string format = ".jpg";
+
+        InputClass input = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath))
+        };
+        
+        inputPath = Path.Combine(dir, "ocr_test_2.pdf");
+        
+        InputClass input2 = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath))
+        };
+        
+        inputPath = Path.Combine(dir, "ocr_test_3.pdf");
+        
+        InputClass input3 = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath))
+        };
+        
+        inputFiles.AddRange([input, input2, input3]);
+
+        try
+        {
+            foreach (InputClass item in inputFiles)
+            {
+                Convert.PdfToPict(item);
+            }
+
+            foreach (string file in Directory.GetFiles(tempDir))
+            {
+                Assert.IsTrue(File.Exists(file));
+                Assert.IsGreaterThan(0, new FileInfo(file).Length);
+            }
+            
+            Assert.HasCount(12, Directory.GetFiles(tempDir));
+        }
+        catch (Exception e)
+        {
+            ErrorLogger.Log(e);
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        finally
+        {
+            //if (Directory.Exists(tempDir))
+                //Directory.Delete(tempDir, true);
+        }
+    }
+    
+    [TestMethod]
+    public void PdfToTxtTest()
+    {
+        string dir = Path.Combine(AppContext.BaseDirectory, "TestData");
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(tempDir);
+        List<InputClass> inputFiles = new List<InputClass>();
+        string inputPath = Path.Combine(dir, "test.pdf");
+        string format = ".txt";
+
+        InputClass input = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath) + format)
+        };
+        
+        inputPath = Path.Combine(dir, "test2.pdf");
+        
+        InputClass input2 = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath) + format)
+        };
+        
+        inputPath = Path.Combine(dir, "test3.pdf");
+        
+        InputClass input3 = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath) + format)
+        };
+        
+        inputFiles.AddRange([input, input2, input3]);
+
+        try
+        {
+            foreach (InputClass item in inputFiles)
+            {
+                Convert.PdfToTxt(item);
+            }
+            
+            foreach (string file in Directory.GetFiles(tempDir))
+            {
+                Assert.IsTrue(File.Exists(file));
+                Assert.IsGreaterThan(0, new FileInfo(file).Length);
+            }
+            
+            Assert.HasCount(3, Directory.GetFiles(tempDir));
+        }
+        catch (Exception e)
+        {
+            ErrorLogger.Log(e);
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        finally
+        {
+            if (Directory.Exists(tempDir))
+                Directory.Delete(tempDir, true);
+        }
+    }
+    
+    [TestMethod]
+    public void PictToTxtTest()
+    {
+        string dir = Path.Combine(AppContext.BaseDirectory, "TestData");
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(tempDir);
+        List<InputClass> inputFiles = new List<InputClass>();
+        string inputPath = Path.Combine(dir, "ocr1.jpg");
+        string format = "";
+
+        InputClass input = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath) + format)
+        };
+        
+        inputPath = Path.Combine(dir, "ocr2.jpg");
+        
+        InputClass input2 = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath) + format)
+        };
+        
+        inputPath = Path.Combine(dir, "ocr3.jpg");
+        
+        InputClass input3 = new InputClass
+        {
+            inputFile = inputPath,
+            outputPath = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(inputPath) + format)
+        };
+        
+        inputFiles.AddRange([input, input2, input3]);
+
+        try
+        {
+            foreach (InputClass item in inputFiles)
+            {
+                Convert.PictToTxt(item);
+            }
+            
+            foreach (string file in Directory.GetFiles(tempDir))
+            {
+                Assert.IsTrue(File.Exists(file));
+                Assert.IsGreaterThan(0, new FileInfo(file).Length);
+            }
+            
+            Assert.HasCount(3, Directory.GetFiles(tempDir));
+        }
+        catch (Exception e)
+        {
+            ErrorLogger.Log(e);
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        finally
+        {
+            if (Directory.Exists(tempDir))
+                Directory.Delete(tempDir, true);
+        }
+    }
+    
+    [TestMethod]
+    public void PictToPdfTest()
+    {
+        string dir = Path.Combine(AppContext.BaseDirectory, "TestData");
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(tempDir);
+        
+        string inputPath = Path.Combine(dir, "ocr1.jpg");
+        string inputPath2 = Path.Combine(dir, "ocr2.jpg");
+        string inputPath3 = Path.Combine(dir, "ocr3.jpg");
+        string[] inputs = new[] { inputPath, inputPath2, inputPath3 };
+        //string format = ".pdf";
+
+        InputClass input = new InputClass
+        {
+            inputFiles = inputs,
+            outputFile = Path.Combine(tempDir, "output.pdf")
+        };
+        
+        try
+        {
+            Convert.PictToPdf(input);
+
+            Assert.IsTrue(File.Exists(input.outputFile));
+            Assert.IsGreaterThan(0, new FileInfo(input.outputFile).Length);
+            
+        }
+        catch (Exception e)
+        {
+            ErrorLogger.Log(e);
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        finally
+        {
+            if (Directory.Exists(tempDir))
+                Directory.Delete(tempDir, true);
+        }
+    }
+}
