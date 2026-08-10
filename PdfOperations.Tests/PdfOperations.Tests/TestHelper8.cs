58ea23d7a75d48f1bc455545f509f814c3ebf7e0
DanBed666 <danielbb0098@gmail.com>
2026-08-10 14:52:51 +0200

    Add test object builders

--- PdfOperations.Tests/PdfOperations.Tests/TestHelper8.cs
+++ PdfOperations.Tests/PdfOperations.Tests/TestHelper8.cs
@@ -2,5 +2,43 @@
 
 public class TestHelper8
 {
+    public static OperationDefinition SetOperationDefinition()
+    {
+        OperationDefinition operationDefinition = new OperationDefinition()
+        {
+
+        };
+
+        return operationDefinition;
+    }
     
+    public static OperationInput SetOperationInput()
+    {
+        OperationInput operationInput = new OperationInput()
+        {
+
+        };
+
+        return operationInput;
+    }
+
+    public static OperationContext SetOperationContext()
+    {
+        OperationContext operationContext = new OperationContext()
+        {
+
+        };
+        
+        return operationContext;
+    }
+
+    public static FileJob SetFileJob()
+    {
+        FileJob fileJob = new FileJob()
+        {
+
+        };
+        
+        return fileJob;
+    }
 }
