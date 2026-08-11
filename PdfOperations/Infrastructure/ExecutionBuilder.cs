396d0dcd0774d3f0b650117f69cb7771d49677ec
DanBed666 <danielbb0098@gmail.com>
2026-08-11 12:56:21 +0200

    Fix no output error in pdf pages

--- PdfOperations/Infrastructure/ExecutionBuilder.cs
+++ PdfOperations/Infrastructure/ExecutionBuilder.cs
@@ -21,15 +21,12 @@ public class ExecutionBuilder
         {
             FileJob fileJob = new FileJob();
             fileJob.InputFile = file;
-            
-            if (operation.OperationFlow == OperationFlow.FilesToFiles)
-            {
-                if (input.InputFiles.Length > 1)
-                    fileJob.TempPath = Files8.PrepareTempPathMultiple(fileJob.InputFile, operationContext, operation);
-                else
-                    fileJob.TempPath = Files8.PrepareTempPathMultipleOne(input, operationContext);
-            }
-            
+
+            if (input.InputFiles.Length > 1)
+                fileJob.TempPath = Files8.PrepareTempPathMultiple(fileJob.InputFile, operationContext, operation);
+            else
+                fileJob.TempPath = Files8.PrepareTempPathMultipleOne(input, operationContext);
+
             fileJob.FinalPath = Files8.PrepareFinalOutputPath(input, fileJob.TempPath);
             fileJobs.Add(fileJob);
         }
