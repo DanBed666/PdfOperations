9639b8a1bcf7fc9849fba2c8ae0f6d20c1e9e01d
DanBed666 <danielbb0098@gmail.com>
2026-09-04 11:02:41 +0200

    Fix page extraction output path

--- PdfOperations/Operations/Pages.cs
+++ PdfOperations/Operations/Pages.cs
@@ -7,7 +7,7 @@ public class Pages
         string tool = ToolPaths.ToolPathsDict[Tool.Qpdf];
         List<string> arguments = new List<string>();
 
-        arguments.AddRange([file.InputFile, "--pages", ".", input.Pages, "--", file.FinalPath]);
+        arguments.AddRange([file.InputFile, "--pages", ".", input.Pages, "--", file.TempPath]);
         RunClass.Run(tool, arguments);
     }
 }
