ca70a9a7d888dfe298716f019b220c3651a31a4b
DanBed666 <danielbb0098@gmail.com>
2026-09-07 14:03:00 +0200

    Fix pdf merge for encrypted files

--- PdfOperations/Operations/Divide.cs
+++ PdfOperations/Operations/Divide.cs
@@ -18,8 +18,30 @@ public class Divide
     {
         string tool = ToolPaths.ToolPathsDict[Tool.PdfUnite];
         List<string> arguments = new List<string>();
-
-        arguments.AddRange([..FileSorter.SortFilesByNumberAndName(file.InputFiles), file.TempPath]);
+        
+        arguments.AddRange([..DecryptFiles(file), file.TempPath]);
         RunClass.Run(tool, arguments);
+    }
+    
+    public static string [] DecryptFiles(FileJob file)
+    {
+        string tool = ToolPaths.ToolPathsDict[Tool.Qpdf];
+        List<string> decrypted = new List<string>();
+        string tempDir = Path.Combine(Path.GetDirectoryName(file.TempPath)!, "decrypted");
+        Directory.CreateDirectory(tempDir);
+        
+        foreach (string f in FileSorter.SortFilesByNumberAndName(file.InputFiles))
+        {
+            List<string> arguments = new List<string>();
+            
+            string unlocked = Path.Combine(tempDir, 
+                Path.GetFileNameWithoutExtension(f) + "_odblokowany" + Path.GetExtension(f));
+            
+            arguments.AddRange(["--decrypt", f, unlocked]);
+            RunClass.Run(tool, arguments);
+            decrypted.Add(unlocked);
+        }
+        
+        return decrypted.ToArray();
     }
 }
