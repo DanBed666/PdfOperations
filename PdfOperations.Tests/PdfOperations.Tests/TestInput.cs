b19c8d56e3810e37c9f108fe46e8b66c777ccd62
DanBed666 <danielbb0098@gmail.com>
2026-09-22 13:50:14 +0200

    Refactor test setup helpers

--- PdfOperations.Tests/PdfOperations.Tests/TestInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PdfOperations.Tests;
-
-public class TestInput
-{
-    public string[] InputFiles { get; set; } = [];
-    public OperationInput Input { get; set; } = new();
-    public OperationContext Context { get; set; } = new();
-    public OperationDefinition Operation { get; set; } = new();
-}
