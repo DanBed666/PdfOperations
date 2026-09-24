0b10962069bccc5485bb9b17b7398e1da8e6d5e6
DanBed666 <danielbb0098@gmail.com>
2026-09-24 11:23:50 +0200

    Test PDF to DOCX conversion

--- PdfOperations.Tests/PdfOperations.Tests/ConvertTests.cs
+++ PdfOperations.Tests/PdfOperations.Tests/ConvertTests.cs
@@ -37,6 +37,49 @@ public class ConvertTests()
     }
     
     [TestMethod]
+    public void PdfToDocxTest()
+    {
+        OperationInput operationInput = new OperationInput()
+        {
+            InputFiles = TestHelper.SetInputPaths(["test_1.pdf", "test_2.pdf", "test_3.pdf"]),
+        };
+
+        OperationContext operationContext = new OperationContext()
+        {
+            TempDir = Files.PrepareTempDir()
+        };
+        
+        OperationDefinition operationDefinition = new OperationDefinition()
+        {
+            Extension = ".docx"
+        };
+        
+        List<FileJob> fileJobList = ExecutionBuilder.SetFileJobsFilesToFiles(operationDefinition, operationInput, operationContext);
+        
+        try
+        {
+            foreach (FileJob fileJob in fileJobList)
+            {
+                Convert.PdfToDocx(fileJob);
+            }
+
+            foreach (string file in Directory.GetFiles(operationContext.TempDir))
+            {
+                Assert.IsTrue(File.Exists(file));
+                Assert.AreEqual(operationDefinition.Extension, Path.GetExtension(file));
+                Assert.IsGreaterThan(0, new FileInfo(file).Length);
+            }
+            
+            Assert.HasCount(3, Directory.GetFiles(operationContext.TempDir));
+        }
+        finally
+        {
+            if (Directory.Exists(operationContext.TempDir))
+                Directory.Delete(operationContext.TempDir, true);
+        }
+    }
+    
+    [TestMethod]
     public void PdfToPictTest()
     {
         OperationInput operationInput = new OperationInput()
